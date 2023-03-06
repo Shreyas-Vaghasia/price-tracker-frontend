c644a8df8b44fb4d5d93e671cd51ad8cab870006
Shreyas Vaghasia <manjuvaghasia0@gmail.com>
2023-03-06 10:47:07 +0530

    fixed Vendors List Action Button's alignment

--- src/pages/AddVendorPage.jsx
+++ src/pages/AddVendorPage.jsx
@@ -302,7 +302,7 @@ const AddVendorPage = () => {
                   ))}
                 </select> */}
                   </div>
-                  <div className='col-md-6'>
+                  <div className='col-md-6 mt-2 mb-2'>
                     <button
                       type='submit'
                       className='btn btn-primary'
@@ -349,38 +349,39 @@ const AddVendorPage = () => {
                       ))}
                     </td> */}
                     <td>
-                      <button
-                        type='button'
-                        className='btn btn-danger p-2'
-                        onClick={() => deleteVendor(vendor.vendorId)}
-                      >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-                          <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
-                        </svg>
+                      <div className="row justify-content-between">
+                        <div className="col-md-4"><button
+                          type='button'
+                          className='btn btn-danger'
+                          onClick={() => deleteVendor(vendor.vendorId)}
+                        >
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                          </svg>
 
-                      </button>
-                    </td>
-                    <td >
-                      <button
-                        type='button'
-                        className='btn btn-primary'
-                        onClick={() => editVendor(vendor.vendorId)}
-                      >
-                        Edit
-                      </button>
-                    </td>
-                    <td >
-                      <button
-                        type='button'
-                        className='btn btn-success'
-                        onClick={() => {
-                          setSelectedVendorForAddingProducts(vendor)
-                          setModalOpen(true)
-                        }}
-                      >
-                        Add
-                      </button>
+                        </button></div>
+                        <div className="col-md-4"><button
+                          type='button'
+                          className='btn btn-primary'
+                          onClick={() => editVendor(vendor.vendorId)}
+                        >
+                          Edit
+                        </button></div>
+                        <div className="col-md-4"><button
+                          type='button'
+                          className='btn btn-success'
+                          onClick={() => {
+                            setSelectedVendorForAddingProducts(vendor)
+                            setModalOpen(true)
+                          }}
+                        >
+                          Add
+                        </button></div>
+                      </div>
+
+
+
                     </td>
                   </tr>
                 ))}
@@ -446,6 +447,8 @@ const AddVendorPage = () => {
           </ModalBody>
 
         </Modal>
+
+
         <Modal isOpen={modalOpen} toggle={() => setModalOpen(false)}>
           <ModalHeader toggle={() => setModalOpen(false)}>Add More Products -{selectedVendorForAddingProducts.vendorName}</ModalHeader>
           <ModalBody>
